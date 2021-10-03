6e1a0ca22c3219ef79cfec0a348efbc065c231d7
quitran86 <quitran86@gmail.com>
2021-10-04 00:13:30 +0700

    Add form register and validation

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirmed Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        from .models.users import Users
+        user = Users.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Please use a different username')
+
+    def validate_email(self, email):
+        from .models.users import Users
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Please use a different email')
